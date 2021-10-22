c9ded9276b5b46383798364703e222ad17652026
Conner Gallagher <ConnerNGallagher@gmail.com>
2021-10-22 09:31:53 -0600

    added mui theme component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,50 +2,23 @@ import React from "react";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import HomepageFeatures from "../components/HomepageFeatures";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { Box } from "@mui/material";
-import {
-  createTheme,
-  ThemeProvider,
-  ThemeOptions,
-  responsiveFontSizes,
-} from "@mui/material/styles";
-import { deepPurple, teal } from "@mui/material/colors";
-
-// export const themeOptions: ThemeOptions = {
-//   palette: {
-//     mode: "light",
-//     primary: {
-//       main: deepPurple[500],
-//     },
-//     secondary: {
-//       main: teal[500],
-//     },
-//     background: {
-//       default: "#232f3e",
-//       // paper: "#232f3e",
-//     },
-//   },
-// };
-let theme = createTheme();
-theme = responsiveFontSizes(theme);
+import Theme from "../theme";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+
   return (
-    <ThemeProvider theme={theme}>
-      <Layout
-        title={`${siteConfig.title}`}
-        description="Documentation for Switchboard V2"
-      >
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Documentation for Switchboard V2"
+    >
+      <Theme>
         <main>
           <HomepageFeatures />
           <Box sx={{ height: 20 }} />
         </main>
-      </Layout>
-    </ThemeProvider>
+      </Theme>
+    </Layout>
   );
 }
---
+++ src/theme.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import useThemeContext from "@theme/hooks/useThemeContext";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import {
+  createTheme,
+  ThemeProvider,
+  ThemeOptions,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+import { deepPurple, teal } from "@mui/material/colors";
+
+const lightThemeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: deepPurple[500],
+    },
+    secondary: {
+      main: teal[500],
+    },
+    background: {
+      default: "#232f3e",
+      // paper: "#232f3e",
+    },
+    text: {
+      primary: "#000000",
+    },
+  },
+};
+const darkThemeOptions: ThemeOptions = {
+  palette: {
+    mode: "dark",
+    primary: {
+      main: deepPurple[500],
+    },
+    text: {
+      primary: "#FFFFFF",
+    },
+  },
+};
+
+let lightTheme = createTheme(lightThemeOptions);
+let darkTheme = createTheme(darkThemeOptions);
+
+export type ThemeProps = {
+  children?: JSX.Element;
+};
+
+export default function Theme(props: ThemeProps): JSX.Element {
+  const { isDarkTheme } = useThemeContext();
+  return (
+    <ThemeProvider
+      theme={
+        isDarkTheme
+          ? responsiveFontSizes(darkTheme)
+          : responsiveFontSizes(lightTheme)
+      }
+    >
+      {props.children}
+    </ThemeProvider>
+  );
+}
