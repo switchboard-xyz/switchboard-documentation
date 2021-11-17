6e19468fb1fec4637e3ffcf3d7fbafddc2525db4
Conner Gallagher <ConnerNGallagher@gmail.com>
2021-11-17 10:38:41 -0700

    added unstyled prop to MarkdownImage component

--- src/components/MarkdownImage.tsx
+++ src/components/MarkdownImage.tsx
@@ -5,26 +5,32 @@ import useThemeContext from "@theme/hooks/useThemeContext";
 
 interface MarkdownImageProps {
   img: string;
+  unstyled?: boolean;
   lightBg?: string;
   darkBg?: string;
 }
 
 const MarkdownImage = (props: MarkdownImageProps) => {
   const { isDarkTheme } = useThemeContext();
+
   let backgroundColor = "inherit";
   if (props.lightBg && !isDarkTheme) backgroundColor = props.lightBg;
   if (props.darkBg && isDarkTheme) backgroundColor = props.darkBg;
+
+  let sx: any = {};
+  if (!props.unstyled) {
+    sx = {
+      backgroundColor,
+      width: "100%",
+      textAlign: "center",
+      borderRadius: 10,
+      boxShadow:
+        "0px 3px 1px -2px pink,0px 2px 2px 0px rgb(99, 91, 255),0px 1px 5px 0px rgb(99, 91, 255)",
+    };
+  }
+
   return (
-    <Box
-      sx={{
-        backgroundColor,
-        width: "100%",
-        textAlign: "center",
-        borderRadius: 10,
-        boxShadow:
-          "0px 3px 1px -2px pink,0px 2px 2px 0px rgb(99, 91, 255),0px 1px 5px 0px rgb(99, 91, 255)",
-      }}
-    >
+    <Box sx={sx}>
       <img src={useBaseUrl(props.img)} />
     </Box>
   );
